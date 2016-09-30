bb641a5869c1fb02980bcc7b0a5e1edf747241a7
Klaus Wong <ocra.klaus@gmail.com>
2016-09-30 12:07:35 +0100

    package.json and server.js for initial environment

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/user/{id}',
+    handler: function (request, reply) {
+        reply('I am user with id:' + encodeURIComponent(request.params.id));
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
