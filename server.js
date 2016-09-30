ccda4d9d30fe22d2e50eb905764e90094def8e5d
Klaus Wong <ocra.klaus@gmail.com>
2016-09-30 16:33:40 +0100

    server.js update

--- server.js
+++ server.js
@@ -9,7 +9,42 @@ server.route({
     method: 'GET',
     path: '/user/{id}',
     handler: function (request, reply) {
-        reply('I am user with id:' + encodeURIComponent(request.params.id));
+        switch (request.params.id) {
+            case '1':
+                var user = {
+                    id: '1',
+                    name: 'Klaus'
+                };
+                reply(user);
+                break;
+            case '2':
+                var user = {
+                    id: '2',
+                    name: 'Asa'
+                };
+                reply(user);
+                break;
+            case '3':
+                var user = {
+                    id: '3',
+                    name: 'Franz'
+                };
+                reply(user);
+                break;
+            case '4':
+                var user = {
+                    id: '4',
+                    name: 'Marcin'
+                };
+                reply(user);
+                break;
+            default:
+                var error = {
+                    code: '404',
+                    message: 'User not found'
+                };
+                reply(error).code(404);
+        }
     }
 });
 
